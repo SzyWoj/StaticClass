7090131f8080d6d8e526aab2705c5c5a19c6efa8
SzyWoj <148621843+SzyWoj@users.noreply.github.com>
2024-03-29 01:03:43 +0100

    Dodaj pliki projektów.

---
+++ Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program!");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Goodbye! Thanks for using me!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using StaticClass;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+        }
+
+    }
+}
